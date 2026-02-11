7d26a807adb32d429b895717a224536d19717428
DonutsK-28 <dal968433@utdallas.edu>
2026-02-11 05:00:43 +0000

    Added font, images, hover effect, and color. Need to remove extra word (cruelty) and somehow merge the hyperlink to the hover word (cruelty).

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
 // main.js
 
 console.log("Hello, World!");
+let targetElement = document.querySelector("h2");
+console.log(targetElement.outerHTML);
+targetElement.addEventListener("mouseover", function() {
+    console.log(this.innerText); 
+    this.innerText = "CRUELTY";
+}); 
+targetElement.addEventListener("mouseout", function() {
+    this.innerText = "IMPERFECTIONS";
+    this.style = "cursor: wait"; 
+    this.style = "cursor: pointer"; 
+})
